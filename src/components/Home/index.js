0600113ac91afd7d93ceab79d7cef372a5bce512
sornekian <sornekian_sarvornek@gmail.com>
2023-03-04 20:37:55 -0800

    finishes home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,30 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss'
 
 const Home = () => {
-    
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+
     return (
         <>
             <div className="container home-page">
                 <div className="text-zone">
-                    <h1>My name is Sarven
+                    <h1>
+                    Hello,
                     <br />
-                    and welcome to my portfolio!
+                    my name is                    
+                    &nbsp;
+                    <span className={`${letterClass} _16`}>Sarven,</span>
                     <br />
+                    welcome to my portfolio!
                     </h1>
                     <h2>Software Engineer / Front End Developer</h2>
                     <Link to="/contact" className='flat-button'>Contact Me</Link>
