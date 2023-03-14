4d911e45c5aba0d2f534db0337f80a3750b9f280
sornekian <sornekian_sarvornek@gmail.com>
2023-03-13 20:36:28 -0700

    updates home page text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,7 +9,9 @@ export default function Home() {
 
   const phraseArray = ['H', 'e', 'l', 'l', 'o', ',', '', 'm', 'y', '', 'n', 'a', 'm', 'e', '', 'i', 's']
 
-  const welcomeArray = ['w', 'e', 'l', 'c', 'o', 'm', 'e', '', 't', 'o', '', 'm', 'y', '', '', 'p', 'o', 'r', 't', 'f', 'o', 'l', 'i', 'o', '!', ''] 
+  const welcomeArray = ['w', 'e', 'l', 'c', 'o', 'm', 'e', '', 't', 'o', '', 'm', 'y', ''] 
+
+  const portfolioArray = ['p', 'o', 'r', 't', 'f', 'o', 'l', 'i', 'o', '!']
 
   useEffect(() => {
     setTimeout(() => {
@@ -35,6 +37,12 @@ export default function Home() {
               strArray={welcomeArray}
               idx={22}
             />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={portfolioArray}
+              idx={22}
+              />
           </h1>
           <h2>Software Engineer / Full Stack Developer</h2>
           <Link to="/contact" className='flat-button'>Contact Me</Link>
