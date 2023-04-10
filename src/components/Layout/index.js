e963162406ccfecf0b1e8c26c85aaf073aa2bfef
sornekian <sornekian_sarvornek@gmail.com>
2023-04-10 11:03:40 -0700

    remove body and html tags

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -6,17 +6,17 @@ export default function Layout() {
   return (
     <div className="App">
       <Sidebar />
-      <div className="page">
-        <span className="tags top-tags">&lt;body&gt;</span>
+      {/* <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span> */}
 
         <Outlet />
 
-        <span className="tags bottom-tags">
+        {/* <span className="tags bottom-tags">
           &lt;/body&gt;
           <br />
           <span className="bottom-tag-html">&lt;/html&gt;</span>
-        </span>
+        </span> */}
       </div>
-    </div>
+    // </div>
   );
 }
