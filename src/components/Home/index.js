2864149ca096e92f969f62f8b603d0dc004fa1ad
sornekian <sarvornek@gmail.com>
2023-11-14 15:00:28 -0800

    adds picture to home page, responsive

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,30 +1,64 @@
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import AnimatedLetters from '../AnimatedLetters';
-import Loader from 'react-loaders';
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Loader from 'react-loaders'
+import selfie from '../../assets/selfie.png'
 import './index.scss'
 
 export default function Home() {
   const [letterClass, setLetterClass] = useState('text-animate')
 
-  const phraseArray = ['H', 'e', 'l', 'l', 'o', ',', '', 'm', 'y', '', 'n', 'a', 'm', 'e', '', 'i', 's']
+  const phraseArray = [
+    'H',
+    'e',
+    'l',
+    'l',
+    'o',
+    ',',
+    '',
+    'm',
+    'y',
+    '',
+    'n',
+    'a',
+    'm',
+    'e',
+    '',
+    'i',
+    's',
+  ]
 
-  const welcomeArray = ['w', 'e', 'l', 'c', 'o', 'm', 'e', '', 't', 'o', '', 'm', 'y', ''] 
+  const welcomeArray = [
+    'w',
+    'e',
+    'l',
+    'c',
+    'o',
+    'm',
+    'e',
+    '',
+    't',
+    'o',
+    '',
+    'm',
+    'y',
+    '',
+  ]
 
   const portfolioArray = ['p', 'o', 'r', 't', 'f', 'o', 'l', 'i', 'o', '!']
 
   useEffect(() => {
     setTimeout(() => {
-      setLetterClass('text-animate-hover');
-    }, 4000);
-  }, []);
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
 
   return (
     <>
       <div className="container home-page">
         <div className="text-zone">
           <h1>
-          <AnimatedLetters
+            <AnimatedLetters
               letterClass={letterClass}
               strArray={phraseArray}
               idx={15}
@@ -42,10 +76,15 @@ export default function Home() {
               letterClass={letterClass}
               strArray={portfolioArray}
               idx={22}
-              />
+            />
           </h1>
           <h2>Software Engineer / Full Stack Developer</h2>
-          <Link to="/contact" className='flat-button'>Contact Me</Link>
+          <Link to="/contact" className="flat-button">
+            Contact Me
+          </Link>
+        </div>
+        <div className="picture-box">
+          <img src={selfie} alt="selfie" />
         </div>
       </div>
       <Loader type="pacman" />
