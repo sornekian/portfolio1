66a9c9ad8e8b49160ccdb57c7a2e1af800e040d6
sornekian <sarvornek@gmail.com>
2024-08-09 16:11:45 -0700

    updates navbar to be on top and changes layout color to mimic space

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,60 +4,48 @@ import LogoS from '../../assets/images/logo-s.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faEnvelope, faPuzzlePiece, faBars, faClose, faFile } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import resume from "../../assets/resume.pdf"
+import resume from "../../assets/resume.pdf";
 import "./index.scss";
-
 
 export default function Sidebar() {
   const [showNav, setShowNav] = useState(false);
 
-
   return (
     <div className='nav-bar'>
-      <Link className='logo' to='/'>
+      {/* <Link className='logo' to='/'>
         <img src={LogoS} alt="logo" />
-      </Link>
+      </Link> */}
       <nav className={showNav ? `mobile-show` : ''}>
-        <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" to="/">
+        <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" to="/" data-text="HOME">
           <FontAwesomeIcon icon={faHome} />
         </NavLink>
-        <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="about-link" to="/about">
+        <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="about-link" to="/about" data-text="ABOUT">
           <FontAwesomeIcon icon={faUser} />
         </NavLink>
-        <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="project-link" to="/projects">
+        <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="project-link" to="/projects" data-text="PROJECTS">
           <FontAwesomeIcon icon={faPuzzlePiece} />
         </NavLink>
-        <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="contact-link" to="/contact">
+        <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="contact-link" to="/contact" data-text="CONTACT">
           <FontAwesomeIcon icon={faEnvelope} />
         </NavLink>
-        <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faClose} color="#FF7F00" size="3x" className="close-icon"
-        />
+        <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faClose} color="#FF7F00" size="3x" className="close-icon" />
       </nav>
       <ul className="socials">
-      <li>
-              <a target="_blank"
-              rel="noreferrer"
-              href={resume}
-              >
-                  <FontAwesomeIcon icon={faFile} color="grey" className="anchor-icon" />
-              </a>
-          </li>
-          <li>
-              <a target="_blank"
-              rel="noreferrer"
-              href="https://www.linkedin.com/in/sarven-ornekian/"
-              >
-                  <FontAwesomeIcon icon={faLinkedin} color="grey" className="anchor-icon" />
-              </a>
-          </li>
-          <li>
-              <a target="_blank"
-              rel="noreferrer"
-              href="https://github.com/sornekian"
-              >
-                  <FontAwesomeIcon icon={faGithub} color="grey" className="anchor-icon" />
-              </a>
-          </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href={resume}>
+            <FontAwesomeIcon icon={faFile} color="grey" className="anchor-icon" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/sarven-ornekian/">
+            <FontAwesomeIcon icon={faLinkedin} color="grey" className="anchor-icon" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://github.com/sornekian">
+            <FontAwesomeIcon icon={faGithub} color="grey" className="anchor-icon" />
+          </a>
+        </li>
       </ul>
       <FontAwesomeIcon onClick={() => setShowNav(true)} icon={faBars} color="#FF7F00" size="3x" className="hamburger-icon" />
     </div>
